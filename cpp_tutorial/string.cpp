0465dd64f176f84bae5441d67f1f614de727c7cf
dun0928 <2464756727@qq.com>
2025-01-26 16:42:17 +0800

    basic2

---
+++ cpp_tutorial/string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string greeting = "Hello";
+  cout << greeting;
+  return 0;
+}
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  cout << "The length of the txt string is: " << txt.size();
+  return 0;
+}
