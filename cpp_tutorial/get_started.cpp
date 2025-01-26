893beea5a9e1274d18422a3cbf31921f14842dba
dun0928 <2464756727@qq.com>
2025-01-26 13:57:56 +0800

    basic

---
+++ cpp_tutorial/get_started.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!";
+    cout<<"I am learning C++";//没有自动的回车，和python不同
+    return 0;
+}
---
+++ cpp_tutorial/getnewline.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << "\n";
+  cout << "I am learning C++";
+  return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!\n";
+//   cout << "I am learning C++";
+//   return 0;
+// }
+
+//or the endl manipulator
