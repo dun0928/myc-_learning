cbf408409f972f2b30d067857b43fa5e85fd0ff7
dun0928 <2464756727@qq.com>
2025-03-10 19:50:37 +0800

    littleend

---
+++ basic_通讯管理.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string s;
+    // cin>>s;
+    // cout<<sizeof(s)<<endl;
+    // cout<<s<<endl;
+    // cout<<"Hello world"<<endl;
+    // system("pause");
+    // cout<<"hello szy";
+    double a = 1.05;
+    double b = 2.0000000005;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
